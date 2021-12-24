0e8ada696e7b6e68aa8b5c674ff227c455fb87c2
Phanord Roberto <phanordpicsouroberto11@gmail.com>
2021-12-24 11:12:58 -0400

    first commit

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -24,8 +24,11 @@ function Home() {
                         <img alt="" src={img} class="rounded-lg" />
                     </div>
                     <div class="">
-                        <h1 class="text-white text-5xl lg:text-6xl tracking-wide font-medium">Hi! I Am</h1>
-                        <h1 class="text-white text-5xl lg:text-6xl font-medium mb-4 leading-normal">Phanord Roberto</h1>
+                        <h1 class="text-white text-5xl lg:text-6xl tracking-wide font-medium">Hello, world</h1>
+                        <h1 class="text-white text-5xl lg:text-6xl font-medium mb-4 leading-normal">I'm Phanord Roberto</h1>
+                        <h1 class="text-white text-5xl lg:text-6xl font-medium mb-4 leading-normal">
+                            UI|UX Designer and Full Stack Developer Web
+                        </h1>
                         <span class="py-4 mt-4 text-gray-300 font-medium leading-relaxed">I am a web developer & programmer living in Santo-Domingo, Dominican Republic. I make web and Cross-platform applications , usually with Reactjs and React native.</span>
                     </div>
                     <button class="cursor-pointer text-white hover:opacity-80 border-1 bg-[#880a0c] px-8 py-3 hover:bg-[#880a0c] rounded-full my-8" onClick={() => openMail()}
