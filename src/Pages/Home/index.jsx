d0e6ae79da3ae191dca22a63bc68770731de32eb
Phanord Roberto <phanordpicsouroberto11@gmail.com>
2021-12-24 14:37:04 -0400

    first commit

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -19,7 +19,6 @@ function Home() {
     return (
         <div id="home" class="relative px-8 mx-auto max-w-7xl">
             <Header />
-            {/* <img alt="" src={circle} class="absolute top-0 right-0" /> */}
             <div class="relative py-16 mx-auto w-full">
                 <div class="flex flex-col lg:flex-row items-center gap-10">
                     <div class="w-full lg:w-1/2">
@@ -34,10 +33,6 @@ function Home() {
                             <h1 class="text-white text-4xl md:text-6xl font-medium mb-4 leading-relaxed">I'm Phanord Roberto, A Full Stack Web Developer</h1>
                             <span class="pt-2 max-w-lg text-xl tracking-wide leading-relaxed text-gray-300">I am a web developer & programmer living in Santo-Domingo, Dominican Republic. I make web and Cross-platform applications , usually with Reactjs and React native.</span>
                         </div>
-                        {/* <button class="cursor-pointer text-white hover:opacity-80 border-1 bg-[#880a0c] px-10 py-2 md:py-4 hover:bg-[#880a0c] rounded-lg my-8" onClick={() => openMail()}
-                        >
-                            Here Me
-                        </button> */}
                         <button class="px-3 md:px-8 my-4 text-white py-4 font-medium tracking-wide bg-[#880a0c] rounded-lg shadow-md transition hover:bg-teal-300 hover:text-black hover:font-semibold"
                             onClick={() => openMail()}
                         >
@@ -48,10 +43,36 @@ function Home() {
                         </div>
                     </div>
                     <div class="hidden lg:relative lg:w-1/2 lg:flex lg:z-10 lg:items-center lg:justify-center">
+                        <div class="absolute top- right-0 flex gap-x-4 p-3 rounded-lg bg-white z-20 items-center w-64">
+                            <img alt="" src={img} 
+                                class="w-12 h-12 rounded-full"
+                            />
+                            <div class="w-50">
+                                <span class="text-xl font-medium">Phanord Roberto</span>
+                                <p class="font-medium text-gray-900">Full-Stack Developer</p>
+                            </div>
+                        </div>
+                        <div class="absolute top-0 left-0 flex gap-x-4 px-5 py-2 rounded-lg bg-white z-20 items-center w-64">
+                            <img alt="" src={img} 
+                                class="w-12 h-12 rounded-full"
+                            />
+                            <div class="w-50">
+                                <span class="text-xl font-medium">Phanord Roberto</span>
+                                <p class="font-medium text-gray-900">Programmer</p>
+                            </div>
+                        </div>
+                        <div class="absolute bottom-0 left-0 flex gap-x-4 p-4 rounded-lg bg-white z-20 items-center w-64">
+                            <img alt="" src={img} 
+                                class="w-12 h-12 rounded-full"
+                            />
+                            <div class="w-50">
+                                <span class="text-xl font-medium">Phanord Roberto</span>
+                                <p class="font-medium text-gray-900">UI|UX Designer</p>
+                            </div>
+                        </div>
                         <img alt="" src={svg1} class="absolute h-full" />
                         <img alt="" src={pic} class="z-10" />
                     </div>
-                        {/* <img alt="" src={design} class="position top-0 right-9 h-40 w-40" /> */}
                 </div>
             </div>
         </div>
